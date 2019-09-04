e0b8e938ab6c2b6d83d2e25bc687c8a193e04df4
Shraddheya <shraddheya.shrivastava@gamil.com>
2019-09-04 19:57:05 +0530

    committing : Wed 09/04/2019_19:57:05.70

--- node/createAngularElectronProjectScript/deploy.bat
+++ node/createAngularElectronProjectScript/deploy.bat
@@ -1,7 +1,13 @@
-REM @echo off
+@echo off
 CLS
 git pull
+echo 
+echo packing
+echo 
 call pkg createAngularElectronProj.js -o createAngularElectronProj.exe
+echo 
+echo deploying
+echo 
 git add .
 git commit -m "committing : %date%_%time% %1"
 git push --set-upstream origin master
