39f6c8702024f01ea7f6fd437f04d7fe8f9249f5
Shraddheya <shraddheya.shrivastava@gamil.com>
2019-09-05 13:25:01 +0530

    include

---
+++ cpp/createCppProj/createCppProj.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <json.hpp>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+    cout << "hello" << NLOHMANN_JSON_VERSION_MINOR << endl;
+    
+    return 0;
+} 
