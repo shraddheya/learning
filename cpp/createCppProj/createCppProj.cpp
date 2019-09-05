d33272c362c2995e8351a92c529ee04bfe983636
Shraddheya <shraddheya.shrivastava@gamil.com>
2019-09-05 14:46:47 +0530

    modifucation

--- cpp/createCppProj/createCppProj.cpp
+++ cpp/createCppProj/createCppProj.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include <json.hpp>
+#include "includes/json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 int main(int argc, const char** argv) {
-    cout << "hello" << NLOHMANN_JSON_VERSION_MINOR << endl;
-    
+    cout << "hello" << endl;
+    for (auto i = 1; i < argc; i++){
+      cout << "argument number " << i << " : " << argv[i] << endl;
+    }
+    system("pause");
     return 0;
 } 
